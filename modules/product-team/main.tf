5565153d863a8cebf3c0beff7e903d42e8cba688
devhulk <gry2big93@gmail.com>
2021-11-27 15:14:52 -0500

    test call mod twice

--- modules/product-team/main.tf
+++ modules/product-team/main.tf
@@ -23,6 +23,7 @@ resource "tfe_workspace" "product_team" {
     organization = var.org
     execution_mode = "remote"
     tag_names = var.workspace_tags
+    queue_all_runs = false
 
     vcs_repo {
         identifier = "devhulk/${var.team_name}"

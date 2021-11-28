650e5050dc02843f75c47a370270a0254f2c4c06
devhulk <gry2big93@gmail.com>
2021-11-27 20:36:43 -0500

    multi workspace test

--- main.tf
+++ main.tf
@@ -47,123 +47,6 @@ module "team_a_prod" {
   vcs_token = var.vcs_token
 }
 
-// resource "tfe_workspace" "product_b" {
-//     name = "product-team-b"
-//     organization = var.org
-//     execution_mode = "remote"
-//     tag_names = ["azure", "prod"]
-
-//     vcs_repo {
-//         identifier = "devhulk/product-team-b"
-//         branch = "main"
-//         oauth_token_id = var.vcs_token
-//     }
-// }
-
-// resource "tfe_run_trigger" "product_b" {
-//   workspace_id  = tfe_workspace.product_b.id
-//   sourceable_id = tfe_workspace.azure_networking.id
-// }
-
-
-// resource "tfe_team_access" "product_b" {
-
-//     team_id      = tfe_team.product_b.id
-//     workspace_id = tfe_workspace.product_b.id
-
-//     permissions {
-//         runs = "apply"
-//         variables = "write"
-//         state_versions = "write"
-//         sentinel_mocks = "read"
-//         workspace_locking = false
-//     }
-// }
-
-
-resource "tfe_team" "networking" {
-  name         = "Networking"
-  organization = var.org
-  visibility = "organization"
-  organization_access {
-      manage_policies = true
-      manage_policy_overrides = true
-      manage_workspaces = true
-      manage_vcs_settings = true
-  }
-
-}
-
-resource "tfe_team" "product_a" {
-  name         = "Product Team A"
-  organization = var.org
-  visibility = "organization"
-  organization_access {
-      manage_policies = false
-      manage_policy_overrides = false
-      manage_workspaces = false
-      manage_vcs_settings = false
-  }
-
-}
-
-
-resource "tfe_team_members" "product_a" {
-    team_id = tfe_team.product_a.id
-    usernames = ["devhulk"]
-}
-
-resource "tfe_team" "product_b" {
-  name         = "Product Team B"
-  organization = var.org
-  visibility = "organization"
-  organization_access {
-      manage_policies = false
-      manage_policy_overrides = false
-      manage_workspaces = false
-      manage_vcs_settings = false
-  }
-
-}
-
-resource "tfe_team_members" "product_b" {
-    team_id = tfe_team.product_b.id
-    usernames = ["devhulk"]
-}
-
-resource "tfe_workspace" "product_a" {
-    name = "product-team-a"
-    organization = var.org
-    execution_mode = "remote"
-    tag_names = ["prod", "team:a"]
-
-    vcs_repo {
-        identifier = "devhulk/product-team-a"
-        branch = "main"
-        oauth_token_id = var.vcs_token
-    }
-}
-
-resource "tfe_run_trigger" "product_a" {
-  workspace_id  = tfe_workspace.product_a.id
-  sourceable_id = tfe_workspace.azure_networking.id
-}
-
-resource "tfe_team_access" "product_a" {
-
-    team_id      = tfe_team.product_a.id
-    workspace_id = tfe_workspace.product_a.id
-
-    permissions {
-        runs = "apply"
-        variables = "write"
-        state_versions = "write"
-        sentinel_mocks = "read"
-        workspace_locking = false
-    }
-}
-
-
 resource "tfe_workspace" "azure_networking" {
     name = "azure-networking"
     organization = var.org
@@ -274,3 +157,107 @@ resource "tfe_team_access" "azure_db_product_a" {
         workspace_locking = false
     }
 }
+
+# resource "tfe_workspace" "product_b" {
+#     name = "product-team-b"
+#     organization = var.org
+#     execution_mode = "remote"
+#     tag_names = ["azure", "prod"]
+
+#     vcs_repo {
+#         identifier = "devhulk/product-team-b"
+#         branch = "main"
+#         oauth_token_id = var.vcs_token
+#     }
+# }
+
+# resource "tfe_run_trigger" "product_b" {
+#   workspace_id  = tfe_workspace.product_b.id
+#   sourceable_id = tfe_workspace.azure_networking.id
+# }
+
+
+# resource "tfe_team_access" "product_b" {
+
+#     team_id      = tfe_team.product_b.id
+#     workspace_id = tfe_workspace.product_b.id
+
+#     permissions {
+#         runs = "apply"
+#         variables = "write"
+#         state_versions = "write"
+#         sentinel_mocks = "read"
+#         workspace_locking = false
+#     }
+# }
+
+# resource "tfe_team" "product_b" {
+#   name         = "Product Team B"
+#   organization = var.org
+#   visibility = "organization"
+#   organization_access {
+#       manage_policies = false
+#       manage_policy_overrides = false
+#       manage_workspaces = false
+#       manage_vcs_settings = false
+#   }
+
+# }
+
+# resource "tfe_team_members" "product_b" {
+#     team_id = tfe_team.product_b.id
+#     usernames = ["devhulk"]
+# }
+
+
+# resource "tfe_team" "product_a" {
+#   name         = "Product Team A"
+#   organization = var.org
+#   visibility = "organization"
+#   organization_access {
+#       manage_policies = false
+#       manage_policy_overrides = false
+#       manage_workspaces = false
+#       manage_vcs_settings = false
+#   }
+
+# }
+
+
+# resource "tfe_team_members" "product_a" {
+#     team_id = tfe_team.product_a.id
+#     usernames = ["devhulk"]
+# }
+
+
+# resource "tfe_workspace" "product_a" {
+#     name = "product-team-a"
+#     organization = var.org
+#     execution_mode = "remote"
+#     tag_names = ["prod", "team:a"]
+
+#     vcs_repo {
+#         identifier = "devhulk/product-team-a"
+#         branch = "main"
+#         oauth_token_id = var.vcs_token
+#     }
+# }
+
+# resource "tfe_run_trigger" "product_a" {
+#   workspace_id  = tfe_workspace.product_a.id
+#   sourceable_id = tfe_workspace.azure_networking.id
+# }
+
+# resource "tfe_team_access" "product_a" {
+
+#     team_id      = tfe_team.product_a.id
+#     workspace_id = tfe_workspace.product_a.id
+
+#     permissions {
+#         runs = "apply"
+#         variables = "write"
+#         state_versions = "write"
+#         sentinel_mocks = "read"
+#         workspace_locking = false
+#     }
+# }
